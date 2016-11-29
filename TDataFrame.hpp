421abc0549d6bac884408a9e8f53de2cb5c699ca
Enrico Guiraud <enrico.guiraud@cern.ch>
2016-11-29 03:31:29 +0100

    first working version of functional chains for ROOT
    root_chains.cpp shows how to use TDataFrame to concatenate filter calls
    and then collect a list of all entries that pass all filters.

---
+++ TDataFrame.hpp
@@ -0,0 +1,149 @@
+#ifndef TDATAFRAME
+#define TDATAFRAME
+
+#include <vector>
+#include <list>
+#include <tuple>
+#include <functional>
+#include <string>
+#include "TTree.h"
+#include "TTreeReaderValue.h"
+#include "TTreeReader.h"
+
+/******* meta-utils **********/
+template<class T>
+struct arg_types {
+   static_assert(sizeof(T) == -1, "this line should never be compiled");
+};
+
+// convert types of arguments of std::function to corresponding std::tuple
+template<class T, class... Args>
+struct arg_types<std::function<T(Args...)>> {
+   using types = typename std::tuple<Args...>;
+};
+
+
+// compile-time integer sequence generator
+// e.g. calling gens<3>::type() instantiates a seq<0,1,2>
+// credits to Johannes Schaub (see http://stackoverflow.com/q/7858817/2195972)
+template<int ...>
+struct seq {};
+
+template<int N, int ...S>
+struct gens : gens<N-1, N-1, S...> {};
+
+template<int ...S>
+struct gens<0, S...>{
+   typedef seq<S...> type;
+};
+/****************************/
+
+
+using BranchList = std::vector<std::string>;
+using EntryList = std::list<unsigned>;
+using TVB = ROOT::Internal::TTreeReaderValueBase;
+template<class... T>
+using FilterLambda = std::function<bool(T...)>; // use as FilterLambda<int,int>
+
+
+// forward declaration (needed by TDataFrame)
+template<class Filter, class PrevData>
+class TTmpDataFrame;
+
+
+class TDataFrame {
+   template<class A, class B> friend class TTmpDataFrame;
+
+   public:
+   TDataFrame(TTree& _t) : t(&_t) {}
+   template<class Filter>
+   auto filter(const BranchList& bl, Filter f) -> TTmpDataFrame<Filter, decltype(*this)> {
+      // Create a TTmpDataFrame that contains *this (and the new filter)
+      return TTmpDataFrame<Filter, decltype(*this)>(t, bl, f, *this);
+   }
+
+   private:
+   bool apply_filters() {
+      // Dummy call (end of recursive chain of calls)
+      return true;
+   }
+   TTreeReader t;
+};
+
+
+template<class Filter, class PrevData>
+class TTmpDataFrame {
+   template<class A, class B> friend class TTmpDataFrame;
+   using arg_types = typename arg_types<Filter>::types;
+   using arg_indexes = typename gens<std::tuple_size<arg_types>::value>::type;
+
+   public:
+   TTmpDataFrame(TTreeReader& _t, const BranchList& _bl, Filter _f,
+                 PrevData& _pd) : t(_t), bl(_bl), f(_f), pd(_pd) {
+      // Call helper function to build vector<TTreeReaderValueBase>
+      build_tvb(arg_indexes());
+   }
+
+   template<class NewFilter>
+   auto filter(const BranchList& bl, NewFilter f) -> TTmpDataFrame<NewFilter, decltype(*this)> {
+      // Create a TTmpDataFrame that contains *this (and the new filter)
+      return TTmpDataFrame<NewFilter, decltype(*this)>(t, bl, f, *this);
+   }
+
+   EntryList collect_entries() {
+      EntryList l;
+      while(t.Next())
+         if(apply_filters(arg_indexes()))
+            l.push_back(t.GetCurrentEntry());   
+      return l;
+   }
+
+   private:
+   template<int...S>
+   void build_tvb(seq<S...>) {
+      // Build vector of pointers to TTreeReaderValueBase. Each element points
+      // to a TTreeReaderValue that corresponds to a different argument of
+      // the filter lambda
+      tvb = { new TTreeReaderValue
+                  < typename std::tuple_element<S, arg_types>::type >
+                  (t, bl[S].c_str())
+              ... // expand over the values of the parameter pack S
+            };
+   }
+
+   template<int...S>
+   bool apply_filters(seq<S...>) {
+      // Recursive call to all previous filters
+      if(!pd.apply_filters())
+         return false;
+
+      // Call our filter
+      // The argument takes each pointer in tvb, casts it to a pointer to the
+      // correct specialization of TTreeReaderValue, and gets its content.
+      // N.B. S expands to a sequence of integers 0..N-1 where N is the number
+      // of arguments taken by f.
+      return f(* (static_cast< TTreeReaderValue< typename std::tuple_element< S, arg_types >::type > * >
+                    (tvb[S])
+                 )->Get()
+               ... // expand over the values of the parameter pack S
+              );
+   }
+
+   bool apply_filters() {
+      return apply_filters(arg_indexes());
+   }
+
+   TTreeReader& t;
+   std::vector<TVB*> tvb;
+   const BranchList& bl;
+   Filter f;
+   PrevData pd;
+};
+
+#endif // TDATAFRAME
+
+/*
+Wish list:
+- build TTreeReaderValues right before looping over TTree
+- only build TTreeReaderValues once per branch
+*/
--- root_chain.cpp
+++ root_chain.cpp
@@ -1,19 +1,20 @@
-#include <string>
-#include <iostream>
 #include "TTree.h"
-#include "TData.hpp"
+#include "TDataFrame.hpp"
 
 void fill_tree(TTree& t);
 
 int main() {
    TTree t("t", "t");
    fill_tree(t);
-   TData d(t);
+   TDataFrame d(t);
    FilterLambda<double> cutb1 = [](double b1) { return b1 < 5.; };
    FilterLambda<int, double> cutb1b2 = [](int b2, double b1) { return b2 % 2 && b1 < 4.; };
    auto entries = d.filter({"b1"}, cutb1)
-                   .filter({"b2","b1"}, cutb1b2);
-   //                .collect_entries();
+                   .filter({"b2","b1"}, cutb1b2)
+                   .collect_entries();
+   
+   for(auto x: entries)
+      std::cout << "entry " << x << " passed all filters" << std::endl;
 
    return 0;
 }
