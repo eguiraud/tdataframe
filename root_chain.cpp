f743646d4fa73ec83b94f9d8aaed117b2fb25f8a
Enrico Guiraud <enrico.guiraud@cern.ch>
2016-12-02 14:56:47 +0100

    add `foreach` example usage to root_chain.cpp

--- root_chain.cpp
+++ root_chain.cpp
@@ -1,5 +1,6 @@
 #include "TTree.h"
 #include "TDataFrame.hpp"
+#include "TH1F.h"
 
 void fill_tree(TTree& t);
 
@@ -10,22 +11,27 @@ int main() {
    // define filters
    auto cutb1 = [](double b1) { return b1 < 5.; };
    auto cutb1b2 = [](int b2, double b1) { return b2 % 2 && b1 < 4.; };
-   // apply filters
+
+   // `collect_entries` action
    auto entries = d.filter({"b1"}, cutb1)
                    .filter({"b2","b1"}, cutb1b2)
                    .collect_entries();
 
-   auto b1_cut = d.filter({"b1"}, cutb1)
-                  .get<double>("b1");
-
-   // print out results
    for(auto x: entries)
       std::cout << "entry " << x << " passed all filters" << std::endl;
+
+   // `get` action
+   auto b1_cut = d.filter({"b1"}, cutb1).get<double>("b1");
 
    std::cout << "\nselected b1 entries" << std::endl;
    for(auto b1_entry: b1_cut)
       std::cout << b1_entry << " ";
    std::cout << std::endl;
+
+   // `foreach` action
+   TH1F h("h", "h", 12, -1, 11);
+   d.filter({"b2"}, [](int b2) { return b2 % 2; })
+    .foreach({"b1"}, [&h](double b1) { h.Fill(b1); });
 
    return 0;
 }
