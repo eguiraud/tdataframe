83367df49007f5c58fa757cc231696c20b1d84aa
bluehood <blue@lcm.mi.infn.it>
2016-12-15 10:33:58 +0100

    first working implementation of forked streams in TDataFrame2.hpp
    TDataFrame simply calls
    
       while(r.Next())
          for(auto actionPtr : fBookedActions)
             actionPtr->Run(r.GetCurrentEntry());
    
    TDataFrameAction checks the previous filter before executing, and each filter
    checks the previous one before applying the filter, in a recursive chain of
    calls that ends at TDataFrame.
    The trick to not apply the same filter twice is caching the result for
    each entry: if a filter is checked a second time for the same entry, it simply
    returns the previous result without executing its lambda and without invoking
    the previous filters, interrupting the recursive chain of calls early.
    
    test2 shows forked streams usage.

--- test2.cpp
+++ test2.cpp
@@ -1,11 +1,51 @@
 #include "TDataFrame2.hpp"
+#include "TFile.h"
+#include "TTree.h"
+#include "ROOT/TSeq.hxx"
 #include <cassert>
+#include <iostream>
+
+// A simple helper function to fill a test tree and save it to file
+// This makes the example stand-alone
+void FillTree(const char* filename, const char* treeName) {
+   TFile f(filename,"RECREATE");
+   TTree t(treeName,treeName);
+   double b1;
+   int b2;
+   t.Branch("b1", &b1);
+   t.Branch("b2", &b2);
+   for(auto i : ROOT::TSeqI(10)) {
+      b1 = i;
+      b2 = i*i;
+      t.Fill();
+   }
+   t.Write();
+   f.Close();
+   return;
+}
 
 int main() {
-   auto dummy = []() { return; };
-   TDataFrame d;
-   d.filter(dummy, {}).foreach(dummy, {});
-   assert(d.fBookedActions.size() == 1);
-   d.foreach(dummy, {});
+   // Prepare an input tree to run on
+   auto fileName = "myfile.root";
+   auto treeName = "myTree";
+   FillTree(fileName,treeName);
+
+   TFile f(fileName);
+   // Define data-frame
+   TDataFrame d(treeName, &f);
+   // ...and two dummy filters
+   auto ok = []() { return true; };
+   auto ko = []() { return false; };
+   d.Filter(ok, {}).Foreach([](double x) { std::cout << x << std::endl; }, {"b1"});
+   d.Run();
+   // Fork stream: always apply first filter before doing two different Foreach
+   auto dd = d.Filter(ok, {});
+   dd.Foreach([](double x) { std::cout << x << " "; }, {"b1"});
+   dd.Foreach([](int y) { std::cout << y << std::endl; }, {"b2"});
+   // Fork again: now apply two filters before performing action
+   auto ddd = dd.Filter(ko, {});
+   ddd.Foreach([]() { std::cout << "ERROR" << std::endl; }, {});
+
+   d.Run();
    return 0;
 }
