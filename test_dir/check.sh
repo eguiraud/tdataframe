92e5b66116181f581efa8721825346d4d102cdb5
bluehood <blue@lcm.mi.infn.it>
2016-12-23 21:22:18 +0100

    add minimal test checking
    just do `cd test_dir; ./check.sh` to see if everything is fine

---
+++ test_dir/check.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+pushd ..
+make
+RES=$?
+if (( $RES > 0 )); then
+   echo "error building tests"
+fi
+popd
+
+FILES=(test2 tdf001_introduction tdf002_dataModel)
+RETCODE=0
+for F in ${FILES[@]}; do
+   ../$F | diff $F.out - > /dev/null
+   RES=$?
+   if (( $RES == 1)); then
+      echo "output for test $F changed!"
+      RETCODE=1
+   elif (( $RES == 2)); then
+      echo "something went wrong diffing outputn of test $F!"
+      RETCODE=2
+   fi
+done
+
+if (( $RETCODE == 0 )); then
+   echo "everything fine!"
+fi
+
+rm -f *.root
+exit $RETCODE
+
